c8ab398d2dfd36b9e4af3afda68abbbcbb9574f5
tomer <tomer.ronen1@mail.huji.ac.il>
2020-07-29 21:12:09 +0300

    1. add some doc strings 2. add basic location tracker 3. divided to more packages

--- app/src/main/java/com/example/wikiaudio/location/LocationTracker.java
+++ app/src/main/java/com/example/wikiaudio/location/LocationTracker.java
@@ -5,19 +5,16 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.pm.PackageManager;
 import android.location.Location;
-import android.location.LocationManager;
 import android.os.Looper;
 
 import androidx.activity.ComponentActivity;
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
 
 import com.google.android.gms.location.FusedLocationProviderClient;
 import com.google.android.gms.location.LocationCallback;
 import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationResult;
 import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 
 public class LocationTracker {
     private Location curLocation;
@@ -52,13 +49,18 @@ public class LocationTracker {
 //    }
 
 
-    public void startTracking() {
+    public Task<Void> startTracking() {
         boolean hasGpsPermissions = ActivityCompat.
                 checkSelfPermission(activity.getApplicationContext(),
                         Manifest.permission.ACCESS_FINE_LOCATION) ==
                         PackageManager.PERMISSION_GRANTED;
         if (hasGpsPermissions) {
-            startLocationUpdates();
+            return startLocationUpdates();
+        }
+        else
+        {
+            // TODO what if there are no GpsPermissions.
+            return null;
         }
     }
 
@@ -78,8 +80,8 @@ public class LocationTracker {
         locationRequest.setPriority(LocationRequest.PRIORITY_BALANCED_POWER_ACCURACY);
     }
 
-    private void startLocationUpdates() {
-        fusedLocationClient.requestLocationUpdates(locationRequest,
+    private Task<Void> startLocationUpdates() {
+        return fusedLocationClient.requestLocationUpdates(locationRequest,
                 locationCallback,
                 Looper.getMainLooper());
     }
