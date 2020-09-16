de3815a5c68de6ce9f5ceae61af4408a66d9593a
Shahrzada <40363696+Shahrzada@users.noreply.github.com>
2020-09-16 14:42:19 +0300

    Launch splash is now working

--- app/src/main/java/com/example/wikiaudio/activates/SplashActivity.java
+++ app/src/main/java/com/example/wikiaudio/activates/SplashActivity.java
@@ -1,15 +1,22 @@
 package com.example.wikiaudio.activates;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.os.SystemClock;
+import android.os.Handler;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SplashActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        finish();
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent i = new Intent(SplashActivity.this, MainActivity.class);
+                startActivity(i);
+                finish();
+            }
+        }, 2500);
     }
 }
